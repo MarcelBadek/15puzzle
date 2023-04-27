46c74fc559c4f8a1ed85b7961b1385bd816c0f27
Marcel Badek <kriollik@gmail.com>
2023-04-27 23:45:33 +0200

    feat: DFS recursively

--- Puzzle.Core/Puzzle.cs
+++ Puzzle.Core/Puzzle.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices.JavaScript;
+using System.Threading.Channels;
 using Puzzle.Core.Heuristics;
 
 namespace Puzzle.Core;
@@ -19,7 +20,7 @@ public class Puzzle
         var visited = new List<Node>();
         const string order = "LURD";
         var iter = 0;
-        
+
         if (mainNode.Board.CheckBoard())
         {
             return;
@@ -90,7 +91,7 @@ public class Puzzle
         {
             return;
         }
-        
+
         openList.Enqueue(mainNode);
 
         while (true)
@@ -102,64 +103,95 @@ public class Puzzle
             // currNode.Board.DisplayBoard();
             // Console.WriteLine();
             // Console.ReadKey();
-            
+
             if (currNode.Board.CheckBoard())
             {
                 return;
             }
-            
-            
+
+
             foreach (var move in order)
             {
                 if (currNode.Move == Helper.GetReverseMove(move))
                 {
                     continue;
                 }
-                
+
                 var board = new Board(currNode.Board);
-                
+
                 if (!board.Move(move))
                 {
                     continue;
                 }
-                
-                 // var alreadyVisited = visited.Any(x => Helper.CompareBoards(x.Board, board));
-                 // if (alreadyVisited)
-                 // {
-                 //     continue;
-                 // }
-                 //
-                 // // ten przypadek istnieje - czeka na sprawdzenie
-                 // var presentInOpenList = openList.Any(x => Helper.CompareBoards(x.Board, board));
-                 // if (presentInOpenList)
-                 // {
-                 //     continue;
-                 // }
+
+                // var alreadyVisited = visited.Any(x => Helper.CompareBoards(x.Board, board));
+                // if (alreadyVisited)
+                // {
+                //     continue;
+                // }
+                //
+                // // ten przypadek istnieje - czeka na sprawdzenie
+                // var presentInOpenList = openList.Any(x => Helper.CompareBoards(x.Board, board));
+                // if (presentInOpenList)
+                // {
+                //     continue;
+                // }
 
                 var newNode = new Node(board, move, currNode);
                 openList.Enqueue(newNode);
             }
         }
     }
-    
-    public void DepthFirstSearch(string order, int maxOrder)
+
+    public bool DepthFirstSearch(string order, int maxDepth, int currentDepth = 0, Node? currNode = null)
     {
-        var mainNode = new Node(Board);
-        var openList = new Queue<Node>();
-        var visited = new List<Node>();
-        var iter = 0;
-
-        if (mainNode.Board.CheckBoard())
+        Node currentNode;
+        if (currentDepth == 0)
         {
-            return;
+            currentNode = new Node(Board);
         }
-        
-        openList.Enqueue(mainNode);
-
-        while (true)
+        else
         {
-            
-            
+            if (currNode is null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                currentNode = currNode;
+            }
         }
+
+        if (currentNode.Board.CheckBoard())
+        {
+            Console.WriteLine("FOUND!!!!!!!!!!!");
+            currentNode.Board.DisplayBoard();
+            return true;
+        }
+
+        Console.WriteLine(currentDepth);
+        if (currentDepth == maxDepth)
+        {
+            return false;
+        }
+
+
+        foreach (var move in order)
+        {
+            var board = new Board(currentNode.Board);
+            if (!board.Move(move))
+            {
+                continue;
+            }
+
+            var newNode = new Node(board, move, currentNode);
+
+            if (DepthFirstSearch(order, maxDepth, currentDepth + 1, newNode))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
